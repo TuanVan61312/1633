3db7464d4c264f16bc442e91049f3df92a8d6a09
TuanVan61312 <vantuan01219230619@gmail.com>
2023-06-01 17:19:07 +0700

    day7

---
+++ Day7/index.js
@@ -0,0 +1,20 @@
+// var x = 5;
+// var y = 6;
+// var z = x + y;
+// alert(z);
+
+// var length = 16;
+// var lastName = "Johnson";
+// var cars = ["Saab", "Volvo", "BMW"];
+// var x = {firstName: "John", lastName: "Doe"};
+
+// var n=20;
+// document.write("Display the odd number <br>");
+// for(i = 0; i < n; i++)
+// {
+//     if(i%2 == 0){
+//         document.write(i + "\t")
+//     }
+// }
+
+document.getElementById("headers").innerHTML = "New header";
